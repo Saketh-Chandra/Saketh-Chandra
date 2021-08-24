8ea2d9da54ece71fc0ed6bea6464e22fda7c013a
Saketh Chandra <b.sakethchandra9@gmail.com>
2021-08-24 21:25:46 +0530

    minor bug fixes

--- Covid-19_Dashboard/main.py
+++ Covid-19_Dashboard/main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 
+from datetime import datetime
 import pandas as pd
 import numpy as np
 from jinja2 import Template
 import sys
+from requests import get
 
 URL_Confirmed = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data' \
                 '/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv '
@@ -11,6 +13,8 @@ URL_Deaths = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/c
              '/csse_covid_19_time_series/time_series_covid19_deaths_global.csv '
 URL_Recovered = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data' \
                 '/csse_covid_19_time_series/time_series_covid19_recovered_global.csv '
+
+URL_Clock_UTC = 'https://www.worldtimeapi.org/api/timezone/Etc/UTC.json'
 
 # -->Confirmed
 dfConfirmed = pd.read_csv(URL_Confirmed)
@@ -48,10 +52,15 @@ _Recovered = dfRecovered.loc[(dfRecovered['Country/Region'] == _Country) & (dfRe
 
 Recovered = _Recovered.to_numpy()
 
+UTC_time = get(URL_Clock_UTC).json()["utc_datetime"]
+dateTime = datetime.strptime(UTC_time, '%Y-%m-%dT%H:%M:%S.%f%z')
+dateTime = dateTime.strftime("%a %d-%b-%y, %H:%M UTC")
+
 template = Template(open('Covid-19_Dashboard/Covid-19_SVG_Template.svg').read())
 out = template.render(Country=_Country, State=_State, index=str(_index[-1]), data_c=f"{int(Confirmed[0][-1]):,d}",
                       data_r=f"{int(Recovered[0][-1]):,d}",
-                      data_d=f"{int(Deaths[0][-1]):,d}")
+                      data_d=f"{int(Deaths[0][-1]):,d}",
+                      dateTime=dateTime)
 
 with open("Covid-19_Dashboard/Covid-19.svg", "w") as outfile:
     outfile.write(out)
