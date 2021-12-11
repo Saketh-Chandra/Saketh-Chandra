2ae488f179a4fb558b10559dd1a36540b4eb2122
Saketh Chandra <b.sakethchandra9@gmail.com>
2021-12-12 05:15:02 +0530

    Minor bug fix: - Show the values only if Confirmed/Recovered/Deaths are greater then 0

--- Covid-19_Dashboard/main.py
+++ Covid-19_Dashboard/main.py
@@ -62,6 +62,5 @@ out = template.render(Country=_Country, State=_State, index=str(_index[-1]), dat
                       data_d=f"{int(Deaths[0][-1]):,d}",
                       dateTime=dateTime)
 
-
 with open("Covid-19_Dashboard/Covid-19.svg", "w") as outfile:
     outfile.write(out)
